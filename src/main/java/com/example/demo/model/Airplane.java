b1dd1791bc426a3b9ab39be55c4f83285fd174f1
Juan Daniel Torres <juanito07604@gmail.com>
2026-09-15 15:28:11 -0500

    Airplanes fixed

--- src/main/java/com/example/demo/model/Airplane.java
+++ src/main/java/com/example/demo/model/Airplane.java
@@ -1,7 +1,25 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "airplanes")
 public class Airplane {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String model;
@@ -9,4 +27,8 @@ public class Airplane {
     private String registrationNumber;
 
     private Integer seatCapacity;
+
+    @ManyToOne
+    @JoinColumn(name = "airline_id", nullable = false)
+    private Airline airline;
 }
