3aee1468d2ce3f26aa3cad111b6b1b32634edaea
Juan Daniel Torres <juanito07604@gmail.com>
2026-09-23 21:11:02 -0500

    ultima consulta

--- src/main/java/com/example/demo/controller/FlightController.java
+++ src/main/java/com/example/demo/controller/FlightController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Airport;
 import com.example.demo.model.Flight;
 import com.example.demo.repository.IFlightRepository;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.Timestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -19,5 +25,10 @@ public class FlightController {
     @GetMapping
     public List<Flight> findAllFlights() {
         return flightRepository.findAll();
+    }
+
+    @GetMapping("/top-airports")
+    public List<Airport> findTopAirports(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime start, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime end) {
+        return flightRepository.findTop5ByArrivalDateBetweenOrderByEstimatedPassengersDesc(Timestamp.valueOf(start), Timestamp.valueOf(end)).stream().map(Flight::getDestinationAirport).distinct().toList();
     }
 }
--- src/main/java/com/example/demo/repository/IFlightRepository.java
+++ src/main/java/com/example/demo/repository/IFlightRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Flight;
+
+import java.util.List;
+
+import java.sql.Timestamp;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IFlightRepository extends JpaRepository<Flight, Long> {
+
+    List<Flight> findTop5ByArrivalDateBetweenOrderByEstimatedPassengersDesc(Timestamp start, Timestamp end);
+
 }
